e6c1278fa92a95dad192c6518eb5957db6a878e6
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2025-07-15 22:04:38 -0300

    TowSum problem

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Problems;
+
+Console.WriteLine("Hello, World!");
+
+int[] result = TowSum.TwoSum([3,3,3,2, 4, 7, 9], 6);
+
+Console.WriteLine(result[0] + " - " + result[1]);
+
---
+++ TowSum.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public class TowSum
+    {
+        public static int[] TwoSum (int[] nums, int target)
+        {
+
+            Dictionary<int, int> indexes = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (indexes.ContainsKey(nums[i]))
+                {
+                    continue;
+                }
+
+                indexes[nums[i]] = i;
+            }
+
+            foreach (int number1 in indexes.Keys)
+            {
+                foreach (int number2 in indexes.Keys)
+                {
+                    Console.WriteLine(number1 + " - " + number2);
+                    if((number1 + number2) == target)
+                    {
+                        return [indexes[number1], indexes[number2]];
+                    }
+                }
+            }
+            return [];
+        }
+
+        public static int[] TwoSumaaa(int[] nums, int target)
+        {
+            Dictionary<int, bool> alreadySum = new Dictionary<int, bool>();
+
+            int[] sumIndex = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (alreadySum.GetValueOrDefault(nums[i], false))
+                {
+                    continue;
+                }
+                alreadySum[nums[i]] = true;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target)
+                    {
+                        sumIndex[0] = i;
+                        sumIndex[1] = j;
+                        return sumIndex;
+                    }
+                }
+            }
+            return sumIndex;
+        }
+    }
+}
